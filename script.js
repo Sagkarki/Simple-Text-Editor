bcf4e960ce6fc5baf03594a3f853da4e922dbf6b
Sagun Karki <sagunkarki111@gmail.com>
2021-05-25 00:29:17 -0400

    Aligned Ftext

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  * DONE: Update the text in the "Formatted Text" section as a user types in the textarea
  * DONE: Add a .bold, .italic classes to "Formatted Text" when the appropriate button is clicked
  * DONE: Add an .underline class to "Formatted Text" when Underline button is clicked
- * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked
+ * DONE: Toggle the align style for "Formatted Text" when the appropriate button is clicked
  */
 
 
@@ -11,7 +11,7 @@
  * HINT: Use the onkeydown function inside HTML
  */
  function updateText(){
-    // CODE GOES HERE
+    
     let text= document.getElementById('text-input').value;
     document.getElementById('text-output').innerText = text;
 }
@@ -24,7 +24,7 @@
    * HINT: Toggle .active class for the button
    */
   function makeBold(elem){
-    //CODE GOES HERE
+   
     elem.classList.toggle('active');
     document.getElementById('text-output').classList.toggle('bold');
   }
@@ -44,7 +44,7 @@
    * HINT: Use contains, remove, and add functions
    */
   function makeUnderline(elem){
-      //CODE GOES HERE
+      
       elem.classList.toggle('active');
       let formattedText= document.getElementById('text-output');
       if (formattedText.classList.contains('underline')){
@@ -61,5 +61,11 @@
    * HINT: Make sure to untoggle the active state for all other align buttons
    */
   function alignText(elem, alignType){
-    // CODE GOES HERE
-  } 
+    
+    document.getElementById('text-output').style.textAlign=alignType;
+    let buttonsList = document.getElementsByClassName('align');
+    for(let i=0; i<buttonsList.length; i++){
+        buttonsList[i].classList.remove('active');
+    }
+    elem.classList.add('active');
+} 
